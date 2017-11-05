056020d35531d3c2014564dfb4e3f2eb76933729
Ciyfhx <pehziheng@gmail.com>
2017-11-05 09:29:51 +0800

    Update

--- MediAid/MediAid/Models/Settings.cs
+++ MediAid/MediAid/Models/Settings.cs
@@ -17,7 +17,7 @@ namespace MediAid.Models
         [PrimaryKey]
         public int SettingsId { get; set; }
 
-        public string Version { get; } = "1.3.5";
+        public string Version { get; } = "1.3.6";
 
         private bool isLogin;
 
